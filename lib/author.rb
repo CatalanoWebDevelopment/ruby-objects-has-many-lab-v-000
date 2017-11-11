587fca9f1949d6550145d14154350900c52a96d4
Parker Catalano <catalano.work@gmail.com>
2017-11-11 22:06:20 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -18,6 +18,13 @@ class Author
     @@post_count += 1
   end
 
+  def add_post_by_title(post)
+    post = Post.new(post)
+    self.posts << post
+    @@post_count += 1
+    post.author = self
+  end
+
   def self.post_count
     @@post_count
   end
